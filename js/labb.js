17957556868c4382ec22e047466c1685d36431a1
Helena <helena@bulle.nu>
2019-04-03 20:13:06 +0200

    forgot bootstrap in my index.html..

--- js/labb.js
+++ js/labb.js
@@ -21,7 +21,6 @@ const req = async () => {
     const res = await fetch('https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple');
     const json = await res.json();
     len = json.results.length;
-    //console.log(json);
     let i = 0;
     while(i < len) {
         let j = 0, v = 0;
@@ -50,3 +49,123 @@ const req = async () => {
         questions.push(resQ);
         i++;
     }
+    // create some variables
+
+    const lastQuestion = questions.length - 1;
+    let runningQuestion = 0;
+    let count = 0;
+    const questionTime = 10; // 10s
+    const gaugeWidth = 150; // 150px
+    const gaugeUnit = gaugeWidth / questionTime;
+    let TIMER;
+    let score = 0;
+
+    // render a question
+    function renderQuestion(){
+        let q = questions[runningQuestion];
+        
+        question.innerHTML = "<p>"+ q.question +"</p>";
+        qImg.innerHTML = "<img src="+ q.imgSrc +">";
+        choiceA.innerHTML = q.choiceA;
+        choiceB.innerHTML = q.choiceB;
+        choiceC.innerHTML = q.choiceC;
+        choiceD.innerHTML = q.choiceD;
+    }
+
+    start.addEventListener("click",startQuiz);
+
+    // start quiz
+    function startQuiz(){
+        start.style.display = "none";
+        renderQuestion();
+        quiz.style.display = "block";
+        renderProgress();
+        renderCounter();
+        TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    }
+
+    // render progress
+    function renderProgress(){
+        for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+            progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+        }
+    }
+
+    // counter render
+
+    function renderCounter(){
+        if(count <= questionTime){
+            counter.innerHTML = count;
+            timeGauge.style.width = count * gaugeUnit + "px";
+            count++
+        }else{
+            count = 0;
+            // change progress color to red
+            answerIsWrong();
+            if(runningQuestion < lastQuestion){
+                runningQuestion++;
+                renderQuestion();
+            }else{
+                // end the quiz and show the score
+                clearInterval(TIMER);
+                scoreRender();
+            }
+        }
+    }
+
+    // checkAnwer
+
+    checkAnswer = function (answer){
+        if( answer == questions[runningQuestion].correct){
+            // answer is correct
+            score++;
+            // change progress color to green
+            answerIsCorrect();
+        }else{
+            // answer is wrong
+            // change progress color to red
+            answerIsWrong();
+        }
+        count = 0;
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+
+    // answer is correct
+    function answerIsCorrect(){
+        document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+    }
+
+    // answer is Wrong
+    function answerIsWrong(){
+        document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    }
+
+    // score render
+    function scoreRender(){
+        scoreDiv.style.display = "block";
+        
+        // calculate the amount of question percent answered by the user
+        const scorePerCent = Math.round(100 * score/questions.length);
+        
+        // choose the image based on the scorePerCent
+        let img = (scorePerCent >= 80) ? "img/5.png" :
+                (scorePerCent >= 60) ? "img/4.png" :
+                (scorePerCent >= 40) ? "img/3.png" :
+                (scorePerCent >= 20) ? "img/2.png" :
+                "img/1.png";
+        
+        scoreDiv.innerHTML = "<img src="+ img +">";
+        scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+    }   
+}
+
+req();
+
+
