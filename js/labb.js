35778726fe8f7722ec4bfb65369a9bbeac178b3e
Helena <helena@bulle.nu>
2019-04-03 20:16:30 +0200

    forgot another thing...

--- js/labb.js
+++ js/labb.js
@@ -147,22 +147,20 @@ const req = async () => {
         document.getElementById(runningQuestion).style.backgroundColor = "#f00";
     }
 
-    // score render
-    function scoreRender(){
-        scoreDiv.style.display = "block";
-        
-        // calculate the amount of question percent answered by the user
-        const scorePerCent = Math.round(100 * score/questions.length);
-        
-        // choose the image based on the scorePerCent
-        let img = (scorePerCent >= 80) ? "img/5.png" :
-                (scorePerCent >= 60) ? "img/4.png" :
-                (scorePerCent >= 40) ? "img/3.png" :
-                (scorePerCent >= 20) ? "img/2.png" :
-                "img/1.png";
-        
-        scoreDiv.innerHTML = "<img src="+ img +">";
-        scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+   // score render
+   function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    let img = (scorePerCent >= 90) ? "img/s3.png" :
+            (scorePerCent >= 60) ? "img/s2.png" :
+            "img/s1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
     }   
 }
 
